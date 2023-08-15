b0c065c58cf5774ea2cc6719e8a0116217150fc2
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-08-15 11:42:11 +0530

    backend add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -39,6 +40,7 @@ import { AlertComponent } from './shared/alert/alert.component';
     MatButtonModule,
     MatDialogModule,
     MatIconModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/bussiness.service.ts
+++ src/app/services/bussiness.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,16 +6,24 @@ import { Injectable } from '@angular/core';
 })
 export class BussinessService {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
   private allPatientData:any;
 
   savePatientData(data:any){
     this.allPatientData=data;
     console.log(this.allPatientData,'from service'); 
+    this.wordFileSaveApi();
   }
 
   getAllPatientData(){
     return this.allPatientData;
+  }
+
+  wordFileSaveApi(){
+    let body=this.allPatientData;
+    this.http.post('http://127.0.0.1:8000/wordfile/',body).subscribe((res:any)=>{
+      console.log(res);
+    });
   }
 
 }
